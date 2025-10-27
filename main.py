dc04f9aab8db9f70def578546ff96388bdfdc8da
atwalnimrat <atwalnimrat5@gmail.com>
2025-10-27 18:08:59 +0530

    process data collection and exploration

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import psutil
 
 from processes import sort_processes
 
-total_cpu_usage_percent = psutil.cpu_percent(interval=1)
+total_cpu_usage_percent = psutil.cpu_percent(interval=0.1)
 total_memory_usage_percent = psutil.virtual_memory().percent
 
-all_processes = psutil.process_iter()
-running_processes, idle_processes, sleeping_processes, stopped_processes, terminated_processes, other_processes = sort_processes(all_processes)
-
 cpu_temp = psutil.sensors_temperatures(fahrenheit=False)['thinkpad'][0]
+
+all_processes = psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent'])
+#running_processes, idle_processes, sleeping_processes, 
+# stopped_processes, terminated_processes, other_processes = sort_processes(all_processes)
+
 
 #print(running_processes[0].cpu_percent())
 #print(running_processes[0].memory_percent())
 
-print(total_cpu_usage_percent)
+
+print(" pid","name", "\t cpu\t", "\tmemory")
+for process in all_processes:
+    memory = process.info['memory_percent']
+    if memory > 0:
+        cpu = process.cpu_percent(interval=0.1)
+        if cpu > 0:
+            print(process.pid, process.name(), cpu, memory)
+
+print("\n\nCPU usage:", total_cpu_usage_percent, "%")
+print("Memory usage:", total_memory_usage_percent, "%")
+print("CPU temperature:", cpu_temp.current, "C")
