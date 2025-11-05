6df09436c3b52a11e1f51035bbb38301e1261b9a
atwalnimrat <atwalnimrat5@gmail.com>
2025-11-05 18:01:19 +0530

    fix: CPU usage discrepancy

---
+++ format.py
@@ -0,0 +1,18 @@
+from rich.table import Table
+
+# For terminal output
+def generate_table(sys_stats, processes):
+    cpu, cores, memory, cpu_temp = sys_stats
+    print(cores)
+    strcores = "%|".join(map(str, cores)) + "%"
+    table = Table(title=f"System Monitor \nCPU: {cpu:.2f}% | Memory: {memory:.2f}% | Temp: {cpu_temp}°C \nCPU Cores: {strcores}")
+
+    table.add_column("PID", justify="right")
+    table.add_column("Name", justify="left", no_wrap=True)
+    table.add_column("CPU%", justify="right")
+    table.add_column("Memory%", justify="right")
+
+    for process in processes:
+        table.add_row(str(process["pid"]), process["name"], f"{process['cpu_percent']:.2f}", f"{process['memory_percent']:.3f}")
+
+    return table
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 from rich.live import Live
 
-from processes import system_stats, process_stats, generate_table
+from processes import system_stats, process_stats
+from format import generate_table
 
 with Live(generate_table(system_stats(), process_stats()), refresh_per_second=1, screen=True) as live:
     while True:
