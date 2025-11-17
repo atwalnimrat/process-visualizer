e3e6b03cb565106b34e0f9289aa5320c494fafa4
atwalnimrat <atwalnimrat5@gmail.com>
2025-11-17 21:07:00 +0530

    fix: removed extra imports

--- gui/bubbles.py
+++ gui/bubbles.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import Qt, QTimer, QPointF
 from PyQt5.QtGui import QPainter, QColor, QFont
 
